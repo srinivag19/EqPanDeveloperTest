528a2956f02409410ee94bd1d43427aa7629ad40
Gopinath Srinivasan <srinivag.cse@gmail.com>
2024-01-24 15:28:24 +0000

    create DAO methods which fetch data from json
    create DAO methods which fetch data from json

--- src/com/pancredit/dao/PaymentDAO.java
+++ src/com/pancredit/dao/PaymentDAO.java
@@ -3,9 +3,21 @@
  */
 package com.pancredit.dao;
 
+import com.pancredit.model.Payment;
+
+import java.util.List;
+
 /**
  * 
  */
 public interface PaymentDAO {
+	
+	List<Payment> getAllPayments();
+
+	void createPayment(Payment payment);
+
+	boolean deletePayment(String paymentId);
+
+	boolean updatePayment(String paymentId, Payment updatedPayment);
 
 }
