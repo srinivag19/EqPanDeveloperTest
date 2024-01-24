4359c341eb274b47605f10ab027be1813b97746d
Gopinath Srinivasan <srinivag.cse@gmail.com>
2024-01-24 15:45:44 +0000

    method to read json data from the file
    method to read json data from the file

--- src/com/pancredit/dao/impl/PaymentDAOImpl.java
+++ src/com/pancredit/dao/impl/PaymentDAOImpl.java
@@ -3,12 +3,63 @@
  */
 package com.pancredit.dao.impl;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pancredit.dao.PaymentDAO;
+import com.pancredit.model.Payment;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * 
  */
 public class PaymentDAOImpl {
+	
+	private List<Payment> payments;
+	private ObjectMapper objectMapper;
+
+	public PaymentDAOImpl() {
+		payments = readDataFromJson();
+		objectMapper = new ObjectMapper();
+	}
+	
+	private List<Payment> readDataFromJson() {
+		List<Payment> result = new ArrayList<>();
+
+		// Get the input stream of the JSON file
+		try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("data.json")) {
+			   System.out.println("path is"+getClass().getClassLoader().getResource("../../resources/data.json"));
+        	   System.out.println("path 2 is"+getClass().getClassLoader().getResource("data.json"));
+        	   System.out.println("path 3 is"+getClass().getClassLoader().getResource("WEB-INF/data.json"));
+        	   System.out.println("path 4 is"+getClass().getClassLoader().getResource("resources"));
+        	   System.out.println("path 4 is"+getClass().getClassLoader().getResource("WEB-INF"));
+               
+
+			if (inputStream != null) {
+				// Use Jackson's ObjectMapper to parse JSON array
+				ObjectMapper objectMapper = new ObjectMapper();
+				result = objectMapper.readValue(inputStream, new TypeReference<List<Payment>>() {
+				});
+
+			}
+
+		} catch (IOException e) {
+			// Handle IOException
+			e.printStackTrace();
+
+		} catch (Exception e) {
+			// Handle other exceptions
+			e.printStackTrace();
+
+		}
+		return result;
+	}
 	
 	
 
