13ecb9cf35d6002acded40a375f8a76d0e8ef079
Gopinath Srinivasan <srinivag.cse@gmail.com>
2024-01-24 15:54:00 +0000

    implement service layer methods to call dao methods
    implement service to call dao methods

--- src/com/pancredit/service/PaymentService.java
+++ src/com/pancredit/service/PaymentService.java
@@ -3,9 +3,38 @@
  */
 package com.pancredit.service;
 
+import com.pancredit.model.Payment;
+import com.pancredit.dao.PaymentDAO;
+
+import java.util.List;
+import java.util.UUID;
+
 /**
  * 
  */
 public class PaymentService {
+
+	private final PaymentDAO paymentDAO;
+
+	public PaymentService(PaymentDAO paymentDAO) {
+		this.paymentDAO = paymentDAO;
+	}
+
+	public List<Payment> getAllPayments() {
+		return paymentDAO.getAllPayments();
+	}
+
+	public void createPayment(Payment payment) {
+		payment.setId(UUID.randomUUID().toString());
+		paymentDAO.createPayment(payment);
+	}
+
+	public boolean deletePayment(String paymentId) {
+		return paymentDAO.deletePayment(paymentId);
+	}
+
+	public boolean updatePayment(String paymentId, Payment updatedPayment) {
+		return paymentDAO.updatePayment(paymentId, updatedPayment);
+	}
 
 }
